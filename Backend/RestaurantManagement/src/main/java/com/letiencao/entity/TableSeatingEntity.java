60965b398b602530826e8b0791d5a35630872c69
Le Tien Cao <caole28072000@gmail.com>
2022-05-27 22:35:51 +0700

    Update file TableSeatingEntity.java

--- Backend/RestaurantManagement/src/main/java/com/letiencao/entity/TableSeatingEntity.java
+++ Backend/RestaurantManagement/src/main/java/com/letiencao/entity/TableSeatingEntity.java
@@ -2,9 +2,6 @@ package com.letiencao.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -15,9 +12,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TableSeatingEntity extends BaseEntity{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
 	@Column(name = "name", columnDefinition = "nvarchar(20)")
 	private String name;
 }
