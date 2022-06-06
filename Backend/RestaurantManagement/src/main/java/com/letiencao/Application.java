2d1d955874dab5fecfd171ee42f0979f0f0952bb
Le Tien Cao <caole28072000@gmail.com>
2022-06-06 17:38:45 +0700

    Update Project

--- Backend/RestaurantManagement/src/main/java/com/letiencao/Application.java
+++ Backend/RestaurantManagement/src/main/java/com/letiencao/Application.java
@@ -1,10 +1,31 @@
 package com.letiencao;
 
+import com.letiencao.dto.request.customer.CustomerInsertionRequest;
+import com.letiencao.dto.request.order.OrderInsertionRequest;
+import com.letiencao.dto.request.reservation.AssignTableRequest;
+import com.letiencao.dto.request.reservation.ReservationInsertionRequest;
+import com.letiencao.dto.request.role.RoleInsertionRequest;
+import com.letiencao.dto.request.tableseating.TableSeatingInsertionRequest;
+import com.letiencao.dto.request.user.UserInsertionRequest;
+import com.letiencao.repository.UserRepository;
+import com.letiencao.service.*;
+import com.letiencao.service.impl.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
+@ComponentScan(basePackages = { "com.letiencao" })
 //@EnableAutoConfiguration(exclude = { //
 //		DataSourceAutoConfiguration.class, //
 //		DataSourceTransactionManagerAutoConfiguration.class, //
@@ -21,11 +42,11 @@ public class Application {
 // 		IReservationService reservationService = context.getBean(ReservationService.class);
 // 		UserRepository userRepository = context.getBean(UserRepository.class);
 // 		IOrderService orderService = context.getBean(OrderService.class);
-
+//
 // 		RoleInsertionRequest roleInsertionRequest = new RoleInsertionRequest();
 // 		roleInsertionRequest.setName("Admin");
 // 		roleService.insertOne(roleInsertionRequest);
-
+//
 // 		List<Integer> roleIds = new ArrayList<Integer>();
 // 		roleIds.add(1);
 // //		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
@@ -39,7 +60,7 @@ public class Application {
 // //			// TODO Auto-generated catch block
 // //			e.printStackTrace();
 // //		}
-
+//
 // //		SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
 // //		try {
 // //			String date = simpleDateFormat1.format(new Date());
@@ -47,7 +68,7 @@ public class Application {
 // ////			System.out.println("Date == " + new Date());
 // //
 // //			Long s = simpleDateFormat1.parse(date).getTime();
-// ////			
+// ////
 // //			s = s + 86399999;
 // //
 // //			System.out.println("Date + 1 == " + simpleDateFormat1.format(s));
@@ -56,11 +77,11 @@ public class Application {
 // //			// TODO Auto-generated catch block
 // //			e.printStackTrace();
 // //		}
-
+//
 // //		Date date = new SimpleDateFormat("03/03/2022 00:00:00");
 // //		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 // //		System.out.println("Date = " + format.format(date));
-
+//
 // //		SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
 // //		try {
 // //			String date = simpleDateFormat1.format(System.currentTimeMillis());
@@ -74,7 +95,7 @@ public class Application {
 // //			// TODO Auto-generated catch block
 // //			e.printStackTrace();
 // //		}
-
+//
 // 		UserInsertionRequest userInsertionRequest = new UserInsertionRequest();
 // 		userInsertionRequest.setFirstName("Cao");
 // 		userInsertionRequest.setLastName("Le");
@@ -91,7 +112,7 @@ public class Application {
 // 		customerInsertionRequest1.setLastName("Chau Viet");
 // 		customerInsertionRequest1.setPhoneNumber("0999899899");
 // 		customerService.insertOne(customerInsertionRequest1);
-
+//
 // //		CustomerInsertionRequest customerInsertionRequest2 = new CustomerInsertionRequest("Trung", "Hoang Chi",
 // //				"0982578392");
 // //		customerService.insertOne(customerInsertionRequest2);
@@ -111,28 +132,28 @@ public class Application {
 // //		CustomerInsertionRequest customerInsertionRequest7 = new CustomerInsertionRequest("Cuong", "Chau Viet",
 // //				"0982578397");
 // //		customerService.insertOne(customerInsertionRequest7);
-
+//
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest.setName("101");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest);
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest1 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest1.setName("102");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest1);
-
+//
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest2 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest2.setName("103");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest2);
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest3 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest3.setName("104");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest3);
-
+//
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest4 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest4.setName("105");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest4);
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest5 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest5.setName("106");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest5);
-
+//
 // 		TableSeatingInsertionRequest tableSeatingInsertionRequest6 = new TableSeatingInsertionRequest();
 // 		tableSeatingInsertionRequest6.setName("107");
 // 		tableSeatingService.insertOne(tableSeatingInsertionRequest6);
@@ -146,7 +167,7 @@ public class Application {
 // 			formatter = new SimpleDateFormat("dd/MM/yyyy");
 // 			date = (Date) formatter.parse(str_date);
 // 			long longDate = date.getTime();
-
+//
 // 			List<Integer> list = new ArrayList<Integer>();
 // 			list.add(1);
 // 			list.add(2);
@@ -162,7 +183,7 @@ public class Application {
 // 			list1.add(1);
 // 			list1.add(2);
 // 			ReservationInsertionRequest reservationInsertionRequest1 = new ReservationInsertionRequest();
-
+//
 // 			reservationInsertionRequest.setCustomerId(1);
 // //			reservationInsertionRequest.setTableSeatingIds(list1);
 // 			reservationInsertionRequest.setDeposit(new BigDecimal(500000));
@@ -170,7 +191,7 @@ public class Application {
 // 			reservationInsertionRequest.setExpectedDate(1651251600000L);
 // //			reservationInsertionRequest.setCaseId(2);
 // 			reservationService.insertOne(reservationInsertionRequest1);
-			
+//
 // 			AssignTableRequest assignTableRequest = new AssignTableRequest();
 // 			assignTableRequest.setReservationId(1);
 // 			List<Integer> integers = new ArrayList<Integer>();
@@ -179,41 +200,41 @@ public class Application {
 // 			assignTableRequest.setTableSeatingIds(integers);
 // 			reservationService.assignTable(assignTableRequest);
 // 			System.out.println("KHÔNG TRY CATCH");
-
+//
 // //			System.out.println("Today is " + longDate);
 // //			System.out.println("Hello = "+System.currentTimeMillis());
 // 		} catch (ParseException e) {
 // 			System.out.println("Exception :" + e);
 // 		}
-
+//
 // 		SimpleDateFormat dtf = new SimpleDateFormat("dd/MM/yyyy");
 // 		System.out.println(dtf.format(System.currentTimeMillis()));
 // 		System.out.println("===========");
 // 		Date date = (Date) dtf.parse(dtf.format(System.currentTimeMillis()));
 // 		long longDate = date.getTime();
 // 		System.out.println("Long date = " + longDate);
-		
+//
 // 		List<Integer> tableSeatingIds = new ArrayList<Integer>();
 // 		tableSeatingIds.add(1);
-		
+//
 // 		OrderInsertionRequest orderInsertionRequest = new OrderInsertionRequest();
 // 		orderInsertionRequest.setUserId(1);
 // 		orderInsertionRequest.setTableSeatingIds(tableSeatingIds);
 // 		orderService.insertOne(orderInsertionRequest);
-		
+//
 // //		List<Integer> tableSeatingIds1 = new ArrayList<Integer>();
 // //		tableSeatingIds1.add(2);
 // //		tableSeatingIds1.add(3);
-// //		
-// //		
-// //		
+// //
+// //
+// //
 // //		OrderInsertionRequest orderInsertionRequest1 = new OrderInsertionRequest();
 // //		orderInsertionRequest1.setUserId(1);
 // //		orderInsertionRequest1.setTableSeatingIds(tableSeatingIds1);
 // //		orderService.insertOne(orderInsertionRequest1);
 // //		System.out.println("USER = " + userRepository.findByEmail("caole28072000@gmail.com"));
-		
-		
+//
+//
 // 		Long longDateNow = System.currentTimeMillis();
 // 		System.out.println("So ngay tinh tu 1/1/1970 = " + longDateNow/86400000);
 	}
