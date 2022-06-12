c2a145b64b9d3d36a70f279c7309f01b848e6482
n.hieu_11 <hieubuimedia@gmail.com>
2022-06-12 22:26:09 +0700

    opentables update
    controller reopen (make sale, update customer) , model

--- controllers/opentables.controller.php
+++ controllers/opentables.controller.php
@@ -29,6 +29,189 @@ class OpenTableController{
         return ModelTables::all();
     }
     
+    public function reopenTable () {
+        
+        if ( !empty($_POST)) {
+            $table = "open_tables";
+            $item = "id";
+            $value = $_GET["id"];
+            
+            $getSale = ModelTables::ShowTablesModel($table, $item, $value);
+            if ($_POST["productsList"] == "") {
+                $productsList = $getSale["products"];
+                $products = json_decode($getSale["products"], true);
+                $productChange = false;
+            } else {
+                $productsList = $_POST["productsList"];
+                $products = json_decode($_POST["productsList"], true);
+                $productChange = true;
+            }
+        }
+        
+        // Make Sale
+        if (isset($_POST["openTable"])) {
+            // if statement checking if the sale was actually changed
+            if ($productChange) {
+                
+                $productsList_2 = json_decode($productsList, true);
+                
+                $totalPurchasedProducts_2 = array();
+                
+                foreach ($productsList_2 as $key => $value) {
+                    
+                    array_push($totalPurchasedProducts_2, $value["quantity"]);
+                    
+                    $tableProducts_2 = "products";
+                    
+                    $item_2 = "id";
+                    $value_2 = $value["id"];
+                    $order = "id";
+                    
+                    $getProduct_2 = ProductsModel::ShowProductsModel($tableProducts_2, $item_2, $value_2, $order);
+                    
+                    $item1a_2 = "sales";
+                    $value1a_2 = $value["quantity"] + $getProduct_2["sales"];
+                    
+                    $newSales_2 = ProductsModel::UpdateProductModel($tableProducts_2, $item1a_2, $value1a_2, $value_2);
+                    
+                    $item1b_2 = "stock";
+                    $value1b_2 = $getProduct_2["stock"] - $value["quantity"];
+                    
+                    $newStock_2 = ProductsModel::UpdateProductModel($tableProducts_2, $item1b_2, $value1b_2, $value_2);
+                    
+                }
+                
+              sModel::UpdateCustomerModel($tableCustomers_2, $item1b_2, $value1b_2, $value_2);
+                
+            }
+            
+            $data = array("code"       => $_POST["reopenSale"],
+                          "idSeller"   => $_POST["idSeller"],
+                          "tableNo"    => $_POST["tableNo"],
+                          "products"   => $productsList,
+                          "netPrice"   => $_POST["newNetPrice"],
+                          //"discount"   => $_POST["newDiscountSale"],
+                          //"totalPrice" => $_POST["newSaleTotal"],
+            );
+            
+            $answer = ModelTables::UpdateTableModel($table, $data);
+            
+            if ($answer == "ok") {
+                
+                echo '<script>
+
+				localStorage.removeItem("range");
+
+				swal({
+					  type: "success",
+					  title: "Update Succesfull",
+					  showConfirmButton: true,
+					  confirmButtonText: "Close"
+					  }).then((result) => {
+								if (result.value) {
+
+								window.location = "open-tables";
+
+								}
+							})
+
+				</script>';
+            
+            }
+        } elseif (isset($_POST["newPaymentMethod"])) {
+
+            // Update customer purchases, stock levels and product sale figures
+            $totalPurchases = array();
+            
+            foreach ($products as $key => $value) {
+                
+                array_push($totalPurchases, $value["quantity"]);
+                
+                $tableProducts = "products";
+                
+                $item = "id";
+                $valueProductId = $value["id"];
+                $order = "id";
+                
+                $getProduct = ProductsModel::ShowProductsModel($tableProducts, $item, $valueProductId, $order);
+                
+                $item1 = "sales";
+                $value1 = $value["quantity"] + $getProduct["sales"];
+                
+                $newSales = ProductsModel::UpdateProductModel($tableProducts, $item1, $value1, $valueProductId);
+                
+                $item2 = "stock";
+                $value2 = $value["stock"];
+                
+                $newStock = ProductsModel::UpdateProductModel($tableProducts, $item2, $value2, $valueProductId);
+                
+            }
+            
+            $tableCustomers = "customers";
+            
+            $item = "idNumber";
+            $valueCustomer = $_POST["customerSearch"];
+            
+            
+            $getCustomer = CustomersModel::ShowCustomersModel($tableCustomers, $item, $valueCustomer);
+            $item1 = "purchases";
+            $value1 = array_sum($totalPurchases) + $getCustomer["purchases"];
+            
+            $customerPurchases = CustomersModel::UpdateCustomerModel($tableCustomers, $item1, $value1, $valueCustomer);
+            
+            $item2 = "lastPurchase";
+            
+            date_default_timezone_set("Europe/Dublin");
+            
+            $date = date('Y-m-d');
+            $hour = date('H:i:s');
+            $value2 = $date . ' ' . $hour;
+            
+            $dateCustomer = CustomersModel::UpdateCustomerModel($tableCustomers, $item2, $value2, $valueCustomer);
+            
+            $table = "sales";
+            
+            $data = array("code"		  => $_POST["reopenSale"],
+                          "idSeller"      => $_POST["idSeller"],
+                          "tableNo"       => $_POST["tableNo"],
+                          "idCustomer"    => $_POST["customerSearch"],
+                          "products"      => $productsList,
+                          "netPrice"      => $_POST["newNetPrice"],
+                          "discount"      => $_POST["newDiscountSale"],
+                          "totalPrice"    => $_POST["newSaleTotal"],
+                          "paymentMethod" => $_POST["newPaymentMethod"]);
+            
+            $answer = ModelSales::AddSaleModel($table, $data);
+            
+            if ($answer == "ok") {
+                
+                ModelSales::DeleteSalesModel('open_tables', $_GET['id']);
+                
+                echo '<script>
+
+				localStorage.removeItem("range");
+
+				swal({
+					  type: "success",
+					  title: "Sale Succesfull",
+					  showConfirmButton: true,
+					  confirmButtonText: "Close"
+					  }).then((result) => {
+								if (result.value) {
+
+								window.location = "sales";
+
+								}
+							})
+
+				</script>';
+            
+            }
+            
+        }
+        
+    }
+
 
     
 }
--- models/opentables.model.php
+++ models/opentables.model.php
@@ -45,4 +45,93 @@ class ModelTables{
 	}
 
 	
+	/**
+	 * inserts a new opentable into the table using input data
+	 * 
+	 * @param mixed $table
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
+	public static function AddTableModel($table, $data){
+
+		$stmt = Connection::connect()->prepare("INSERT INTO $table(code, idSeller, tableNo, products, netPrice) VALUES (:code, :idSeller, :tableNo, :products, :netPrice)");
+
+		$stmt->bindParam(":code", $data["code"], PDO::PARAM_INT);
+		$stmt->bindParam(":idSeller", $data["idSeller"], PDO::PARAM_INT);
+		$stmt->bindParam(":tableNo", $data["tableNo"], PDO::PARAM_STR);
+		$stmt->bindParam(":products", $data["products"], PDO::PARAM_STR);
+		$stmt->bindParam(":netPrice", $data["netPrice"], PDO::PARAM_STR);
+		
+		if($stmt->execute()){
+
+			return "ok";
+
+		}else{
+
+			return "error";
+		
+		}
+
+		$stmt->close();
+		$stmt = null;
+
+	}
+
+	
+	/**
+	 * updates the opentable by code
+	 * 
+	 * @param mixed $table
+	 * @param mixed $data
+	 * 
+	 * @return void
+	 */
+	public static function UpdateTableModel($table, $data){
+
+		$stmt = Connection::connect()->prepare("UPDATE $table SET idSeller = :idSeller, tableNo = :tableNo, products = :products, netPrice= :netPrice WHERE code = :code");
+
+		$stmt->bindParam(":code", $data["code"], PDO::PARAM_INT);
+		$stmt->bindParam(":idSeller", $data["idSeller"], PDO::PARAM_INT);
+		$stmt->bindParam(":tableNo", $data["tableNo"], PDO::PARAM_STR);
+		$stmt->bindParam(":products", $data["products"], PDO::PARAM_STR);
+		$stmt->bindParam(":netPrice", $data["netPrice"], PDO::PARAM_STR);
+		
+		if($stmt->execute()){
+
+			return "ok";
+
+		}else{
+
+			return "error";
+		
+		}
+
+		$stmt->close();
+		$stmt = null;
+
+	}
+	
+	/**
+	 * displays all opentables in the opentable table
+	 * @return void
+	 */
+	public static function all () {
+        $stmt = Connection::connect()->prepare("SELECT open_tables.* FROM open_tables ORDER BY id ASC");
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        return $stmt->fetchAll();
+	}
+    
+    /**
+	 * displays code sakes table
+	 * @return void
+	 */
+    public static function getCode () {
+        $stmt = Connection::connect()->prepare("SELECT MAX(code) AS code FROM sales ");
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        return $stmt->fetch();
+	}
+	
 }
