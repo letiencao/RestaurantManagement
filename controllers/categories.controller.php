42056b1088a7655f5dab958093ca0274ccf16683
khangvt187323 <khang.vt187323@sis.hust.edu.vn>
2022-06-13 22:07:52 +0700

    add edit and show categories

--- controllers/categories.controller.php
+++ controllers/categories.controller.php
@@ -71,3 +71,92 @@
 			}
 		}
     }
+
+    // Show Categories
+	/**
+	 * fetches the categories table from the database
+	 * then displays the contents 
+	 * 
+     * @param mixed $item
+     * @param mixed $value
+     * 
+     * @return void
+     */
+    public static function ShowCategoriesController($item, $value){
+
+		$table = "categories";
+
+		$answer = CategoriesModel::ShowCategoriesModel($table, $item, $value);
+
+		return $answer;
+    }
+	
+	/**
+	 * edits the category selected if the new inputs dont contain any invalid characters
+	 * if invalid chars are present the editing will fail and the user will be given an error message
+	 * success message if edited successfully
+	 * 
+     * @return void
+     */
+    public static function EditCategoryController(){
+
+		if(isset($_POST["editCategory"])){
+
+			if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["editCategory"])){
+
+				$table = "categories";
+
+				$data = array("Category"=>$_POST["editCategory"],
+							   "id"=>$_POST["idCategory"],
+							   "Vat"=>$_POST["newVat"],
+							   "Tax"=>$_POST["newTax"]);
+
+				$answer = CategoriesModel::EditCategoryModel($table, $data);
+
+				if($answer == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "Category Edited Successfully",
+						  showConfirmButton: true,
+						  confirmButtonText: "Close"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "categories";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "Please fill in all fields, no special characters allowed",
+						  showConfirmButton: true,
+						  confirmButtonText: "Close"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "categories";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+    }
+ }
